84cb3132638888f5f089bb8b5be43c23dceff7db
Zainab Habas <zainab.habas@telecom-paristech.fr>
2017-06-16 16:39:47 +0200

    traitement du frame corpus

--- clustering.py
+++ clustering.py
@@ -5,3 +5,26 @@ Created on Fri Jun 16 11:34:06 2017
 @author: zainabhabas
 """
 
+import numpy as np 
+from sklearn.cluster import KMeans
+
+fp = open("/Users/zainabhabas/Documents/workspace/Comprendre-et-raisonner-pour-un-personnage-virtuel/numberbatch-en-17.04b.txt", "r",encoding='UTF8')
+
+words = []
+coordinates = {}
+i = 0
+
+for line in fp:
+    
+    t = line.split()
+    print(t)
+    words = words + [t[0]]
+    print(words)
+    coordinates[i] = t[1:300]
+    i+=i
+         
+fp.close()
+
+X = np.matrix(zip(words, coordinates))
+
+kmeans = KMeans(n_clusters=30, random_state=0).fit(X)
