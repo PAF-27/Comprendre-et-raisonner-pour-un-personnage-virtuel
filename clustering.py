5011f9e8e6be3b8366b2bcafd6e9cb3b8f07dec8
clandrin <clement.landrin@telecom-paristech.fr>
2017-06-16 17:44:43 +0200

    code de clusterisation à partir de la base de données de numberbatch anglaise

--- clustering.py
+++ clustering.py
@@ -5,3 +5,34 @@ Created on Fri Jun 16 11:34:06 2017
 @author: zainabhabas
 """
 
+import numpy as np 
+from sklearn.cluster import KMeans
+
+f= open('C:\\Users\\cleme\\numberbatch-en-17.04b.txt','r',encoding='UTF8')
+#f= open('C:\\Users\\cleme\\Desktop\\test.txt','r',encoding='UTF8')
+words=[]
+tmp=[]
+coordinates=[]
+f.readline()
+tmp2=[]
+i=0
+for line in f:
+    t=line.split()
+    i=i+1
+    print(i)
+    for j in range (1, len(t)) :
+        string=t[j]
+        if string.startswith('-'):
+            string=string.replace('-','')
+            float(string)
+            t[j]=-float(string)
+        else :
+            t[j]=float(string)
+    words.append(t[0])
+    tmp=t[1:len(t)]
+    coordinates.append(tmp)
+#X=zip(words, coordinates)
+#Y = np.matrix(X)
+kmeans = KMeans(n_clusters=2, random_state=0).fit(coordinates)
+print(kmeans.labels_)
+print(kmeans.cluster_centers_)
