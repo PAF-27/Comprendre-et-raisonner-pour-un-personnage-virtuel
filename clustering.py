f6312f3c7f27ff06b1e10a647e4bac97f013fa7b
clandrin <clement.landrin@telecom-paristech.fr>
2017-06-19 18:22:43 +0200

    mise en forme du code sous forme de fonction. Creation d'un code permettant de visualiser les distances moyennes en fonction du nombre de clusters.

--- clustering.py
+++ clustering.py
@@ -7,10 +7,12 @@ Created on Fri Jun 16 11:34:06 2017
 
 import numpy as np 
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as pl
 
-
+    
+    
+#permet de décoder le fichier texte contenant la projection des frames sur un espace en 300 dismension
 f= open("C:\\Users\\cleme\git\\Comprendre-et-raisonner-pour-un-personnage-virtuel\\resultat.txt",'r',encoding='UTF8')
-
 words=[]
 tmp=[]
 coordinates=[]
@@ -32,52 +34,99 @@ for line in f:
     words.append(t[0])
     tmp=t[1:len(t)]
     coordinates.append(tmp)
+#//////////////////////////////////////////////////////////////////////////////
 
 
 
+#fonction de calcul de distance
 def distance(n, x, y):
     res=0
     for i in range(n):
          res=res+abs(float(x[i])-float(y[i]))
     return res
-    
-    
-    
-n=10
-kmeans = KMeans(n_clusters=n, random_state=0).fit(coordinates)
-print(kmeans.labels_)
-print(kmeans.cluster_centers_)
-clustersParMots=[]
-clustersParValeurs=[]
+#///////////////////////////////////////////////////////////////////////////
 
 
-for i in range (n):
-    listeClusterIParMots=[]
-    listeClusterIParValeurs=[]
-    for j in range(len(kmeans.labels_)) :
-        if kmeans.labels_[j]==i:
-            listeClusterIParMots.append(words[j])
-            listeClusterIParValeurs.append(coordinates[j])
-    clustersParValeurs.append(listeClusterIParValeurs)
-    clustersParMots.append(listeClusterIParMots)
-    
-    
-print(clustersParMots)
-minimum=[]
-positionCentre=[0 for i in range(n)]
-distanceMoyenne=[0 for i in range(n)]
-for i in range(n):
-    minimum.append(distance(300, clustersParValeurs[i][0], kmeans.cluster_centers_[i]))
-    for j in range(len(clustersParValeurs[i])):
-        distanceTmp=distance(300,clustersParValeurs[i][j],kmeans.cluster_centers_[i])
-        if distanceTmp<minimum[i]:
-            positionCentre[i]=j
-        distanceMoyenne[i]=distanceMoyenne[i]+distanceTmp
-    distanceMoyenne[i]=distanceMoyenne[i]/len(clustersParValeurs[i])
-print(distanceMoyenne)
-print(positionCentre)
-centreString=""
-for i in range(n):
-    centreString=centreString+" "+words[positionCentre[i]]
 
-print(centreString)
+
+
+
+#permet de retourner la liste des clusters par mots ou par valeurs
+def trouverCluster(n,kmeans):
+    clustersParValeurs=[]
+    clustersParMots=[]
+    for i in range (n):
+        listeClusterIParMots=[]
+        listeClusterIParValeurs=[]
+        for j in range(len(kmeans.labels_)) :
+            if kmeans.labels_[j]==i:
+                listeClusterIParMots.append(words[j])
+                listeClusterIParValeurs.append(coordinates[j])
+        clustersParValeurs.append(listeClusterIParValeurs)
+        clustersParMots.append(listeClusterIParMots)
+    return clustersParValeurs,clustersParMots
+#///////////////////////////////////////////////////////////////
+
+
+
+#permet de calculer le centre et la distance moyenne pour chaque cluster
+def trouverDistanceEtCentre(n,clustersParValeurs,kmeans):
+    minimum=[]
+    positionCentre=[0 for i in range(n)]
+    distanceMoyenne=[0 for i in range(n)]
+    for i in range(n):
+        minimum.append(distance(300, clustersParValeurs[i][0], kmeans.cluster_centers_[i]))
+        for j in range(len(clustersParValeurs[i])):
+            distanceTmp=distance(300,clustersParValeurs[i][j],kmeans.cluster_centers_[i])
+            if distanceTmp<minimum[i]:
+                positionCentre[i]=j
+            distanceMoyenne[i]=distanceMoyenne[i]+distanceTmp
+        distanceMoyenne[i]=distanceMoyenne[i]/len(clustersParValeurs[i])
+    centreString=""
+    for i in range(n):
+        centreString=centreString+" "+words[positionCentre[i]]
+    return centreString, distanceMoyenne
+#//////////////////////////////////////////////////////////////
+
+
+
+
+
+
+#permet de trouver les clusters avec un paramètre entier n 
+def KMEANS(n):
+    kmeans = KMeans(n_clusters=n, random_state=0).fit(coordinates)
+    clustersParValeurs,clustersParMots=trouverCluster(n,kmeans)
+    centreString,distanceMoyenne=trouverDistanceEtCentre(n,clustersParValeurs,kmeans)
+    return clustersParValeurs,clustersParMots,centreString,distanceMoyenne
+#///////////////////////////////////////////////////////////////
+
+#permet de comparer les distances moyennes entre les différentes clusterisations
+def comparaisonDistanceMoyenne(jusqua):
+    listeDistanceMoyenne=[]
+    for i in range(1,jusqua+1):
+        valeurMoyenne=0
+        listeDesDistanceDeI=KMEANS(i)[3]
+        for j in range(i):
+            valeurMoyenne=listeDesDistanceDeI[j]+valeurMoyenne
+        valeurMoyenne=valeurMoyenne/i
+        listeDistanceMoyenne.append(valeurMoyenne)
+    return listeDistanceMoyenne
+
+def trouverMin(liste):
+    indicemin=0
+    valeurmin=liste[indicemin]
+    for i in range(len(liste)):
+        if liste[i]<valeurmin:
+            indicemin=i
+    return i
+
+n=100
+pl.plot([i for i in range(1, n+1)], comparaisonDistanceMoyenne(n))
+pl.show()
+
+
+
+        
+
+
