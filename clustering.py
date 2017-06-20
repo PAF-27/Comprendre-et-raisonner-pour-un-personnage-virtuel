e645145dbe0bb1ef881755e350993efa35a531e0
clandrin <clement.landrin@telecom-paristech.fr>
2017-06-20 10:35:31 +0200

    ajout des differentes distances (euclidienne, Minkowski et Tchebychev)

--- clustering.py
+++ clustering.py
@@ -47,8 +47,19 @@ def distance(n, x, y):
 #///////////////////////////////////////////////////////////////////////////
 
 
+#fonction de calcul de distance de minkowski
+def distanceMinkowski(n,x,y,p):
+    res=0
+    for i in range(n):
+         res=res+(float(x[i])-float(y[i]))**p
+    return res**(1/p)
+#////////////////////////////////////////////////////////////////////////
 
-
+def distanceTchebychev(n,x,y):
+    distanceParCoordonnees=[0 for i in range(n)]
+    for i in range(n):
+        distanceParCoordonnees[i]=abs(x[i]-y[i])
+    return max(distanceParCoordonnees[i] for i in range(n))
 
 
 #permet de retourner la liste des clusters par mots ou par valeurs
@@ -89,9 +100,43 @@ def trouverDistanceEtCentre(n,clustersParValeurs,kmeans):
 #//////////////////////////////////////////////////////////////
 
 
+#permet de calculer le centre et la distance moyenne pour chaque cluster à partir de la distance de minkowski avec p un paramètre
+def trouverDistanceEtCentreMinkowski(n,clustersParValeurs,kmeans,p):
+    minimum=[]
+    positionCentre=[0 for i in range(n)]
+    distanceMoyenne=[0 for i in range(n)]
+    for i in range(n):
+        minimum.append(distanceMinkowski(300, clustersParValeurs[i][0], kmeans.cluster_centers_[i],p))
+        for j in range(len(clustersParValeurs[i])):
+            distanceTmp=distanceMinkowski(300,clustersParValeurs[i][j],kmeans.cluster_centers_[i],p)
+            if distanceTmp<minimum[i]:
+                positionCentre[i]=j
+            distanceMoyenne[i]=distanceMoyenne[i]+distanceTmp
+        distanceMoyenne[i]=distanceMoyenne[i]/len(clustersParValeurs[i])
+    centreString=""
+    for i in range(n):
+        centreString=centreString+" "+words[positionCentre[i]]
+    return centreString, distanceMoyenne
+#//////////////////////////////////////////////////////////////
 
-
-
+#permet de calculer le centre et la distance moyenne pour chaque cluster à partir de la distance de Tchebychev
+def trouverDistanceEtCentreTchebychev(n,clustersParValeurs,kmeans):
+    minimum=[]
+    positionCentre=[0 for i in range(n)]
+    distanceMoyenne=[0 for i in range(n)]
+    for i in range(n):
+        minimum.append(distanceTchebychev(300, clustersParValeurs[i][0], kmeans.cluster_centers_[i]))
+        for j in range(len(clustersParValeurs[i])):
+            distanceTmp=distanceTchebychev(300,clustersParValeurs[i][j],kmeans.cluster_centers_[i])
+            if distanceTmp<minimum[i]:
+                positionCentre[i]=j
+            distanceMoyenne[i]=distanceMoyenne[i]+distanceTmp
+        distanceMoyenne[i]=distanceMoyenne[i]/len(clustersParValeurs[i])
+    centreString=""
+    for i in range(n):
+        centreString=centreString+" "+words[positionCentre[i]]
+    return centreString, distanceMoyenne
+#//////////////////////////////////////////////////////////////
 
 #permet de trouver les clusters avec un paramètre entier n 
 def KMEANS(n):
@@ -100,6 +145,23 @@ def KMEANS(n):
     centreString,distanceMoyenne=trouverDistanceEtCentre(n,clustersParValeurs,kmeans)
     return clustersParValeurs,clustersParMots,centreString,distanceMoyenne
 #///////////////////////////////////////////////////////////////
+
+
+#permet de trouver les clusters avec un paramètre entier n ainsi qu'un paramètre p selon la distance de Minkowski
+def KMEANSMinkowski(n,p):
+    kmeans = KMeans(n_clusters=n, random_state=0).fit(coordinates)
+    clustersParValeurs,clustersParMots=trouverCluster(n,kmeans)
+    centreString,distanceMoyenne=trouverDistanceEtCentreMinkowski(n,clustersParValeurs,kmeans,p)
+    return clustersParValeurs,clustersParMots,centreString,distanceMoyenne
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#permet de trouver les clusters avec un paramètre entier n selon la distance de Tchebychev
+def KMEANSTchebychev(n):
+    kmeans = KMeans(n_clusters=n, random_state=0).fit(coordinates)
+    clustersParValeurs,clustersParMots=trouverCluster(n,kmeans)
+    centreString,distanceMoyenne=trouverDistanceEtCentreTchebychev(n,clustersParValeurs,kmeans)
+    return clustersParValeurs,clustersParMots,centreString,distanceMoyenne
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #permet de comparer les distances moyennes entre les différentes clusterisations
 def comparaisonDistanceMoyenne(jusqua):
@@ -112,6 +174,34 @@ def comparaisonDistanceMoyenne(jusqua):
         valeurMoyenne=valeurMoyenne/i
         listeDistanceMoyenne.append(valeurMoyenne)
     return listeDistanceMoyenne
+#//////////////////////////////////////////////////////////////////////////////
+
+#permet de comparer les distances moyennes entre les différentes clusterisations selon la distance de Minkowski avec un parametre p
+def comparaisonDistanceMoyenneMinkowski(jusqua,p):
+    listeDistanceMoyenne=[]
+    for i in range(1,jusqua+1):
+        valeurMoyenne=0
+        listeDesDistanceDeI=KMEANSMinkowski(i,p)[3]
+        for j in range(i):
+            valeurMoyenne=listeDesDistanceDeI[j]+valeurMoyenne
+        valeurMoyenne=valeurMoyenne/i
+        listeDistanceMoyenne.append(valeurMoyenne)
+    return listeDistanceMoyenne
+#//////////////////////////////////////////////////////////////////////////////
+
+#permet de comparer les distances moyennes entre les différentes clusterisations selon la distance de Minkowski avec un parametre p
+def comparaisonDistanceMoyenneTchebychev(jusqua):
+    listeDistanceMoyenne=[]
+    for i in range(1,jusqua+1):
+        valeurMoyenne=0
+        listeDesDistanceDeI=KMEANSTchebychev(i)[3]
+        for j in range(i):
+            valeurMoyenne=listeDesDistanceDeI[j]+valeurMoyenne
+        valeurMoyenne=valeurMoyenne/i
+        listeDistanceMoyenne.append(valeurMoyenne)
+    return listeDistanceMoyenne
+#//////////////////////////////////////////////////////////////////////////////
+
 
 def trouverMin(liste):
     indicemin=0
@@ -121,8 +211,9 @@ def trouverMin(liste):
             indicemin=i
     return i
 
-n=100
-pl.plot([i for i in range(1, n+1)], comparaisonDistanceMoyenne(n))
+n=561
+p=2
+pl.plot([i for i in range(1, n+1)], comparaisonDistanceMoyenneTchebychev(n))
 pl.show()
 
 
